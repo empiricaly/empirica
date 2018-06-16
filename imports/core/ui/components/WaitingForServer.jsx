b3d51be2c1c7a8967c74341a692c3db8701b04f5
Nicolas Paton <nicolas.paton@gmail.com>
2018-06-16 17:01:57 +0800

    Typo

--- imports/core/ui/components/WaitingForServer.jsx
+++ imports/core/ui/components/WaitingForServer.jsx
@@ -9,11 +9,11 @@ export default class WaitingForServer extends React.Component {
             <span className="pt-icon pt-icon-automatic-updates" />
           </div>
           <h4 className="pt-non-ideal-state-title">
-            {/*a more neutral message in case it was a single player*/}
+            {/* A more neutral message in case it was a single player */}
             Waiting for server response...
           </h4>
           <div className="pt-non-ideal-state-description">
-            Please wait until all players are ready. if this takes more than 5
+            Please wait until all players are ready. If this takes more than 5
             seconds, please <em>Refresh the page</em>.
           </div>
         </div>
