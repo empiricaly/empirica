1d829846b50566942acb8c6c0d3e9b2dbe9be024
Nicolas Paton <nicolas.paton@gmail.com>
2018-09-06 16:33:07 +0800

    Fix admin credentials. Fixes #16.

--- imports/core/startup/server/bootstrap.js
+++ imports/core/startup/server/bootstrap.js
@@ -1,12 +1,42 @@
 import { Conditions } from "../../api/conditions/conditions.js";
 import { config } from "../../../experiment/server";
+import log from "../../lib/log.js";
 
-const admins = [
-  {
-    username: "admin",
-    password: "victoria newish cymbal easter"
+const admins = [];
+
+const settingsAdmins = Meteor.settings.admins;
+if (settingsAdmins) {
+  if (!_.isArray(settingsAdmins)) {
+    log.error("settings: `admins` field is not an array");
+  } else {
+    settingsAdmins.forEach(({ username, password }) => {
+      if (!username || !password) {
+        log.error("settings: `admins` require `username` and `password`");
+      } else {
+        admins.push({ username, password });
+      }
+    });
   }
-];
+}
+
+if (admins.length === 0) {
+  const tempPassword =
+    Math.random()
+      .toString(36)
+      .slice(2) +
+    Math.random()
+      .toString(36)
+      .slice(2);
+
+  admins.push({
+    username: "admin",
+    password: tempPassword
+  });
+
+  log.warn(
+    `You have not set any custom passwords in your settings file (see Readme), you can temporarily log in with (reset on each app reload):\n- username: admin\n- password: ${tempPassword}\n`
+  );
+}
 
 const bootstrapFunctions = [];
 export const bootstrap = () => {
@@ -22,6 +52,8 @@ bootstrapFunctions.push(() => {
     const exists = Meteor.users.findOne(_.omit(admin, "password"));
     if (!exists) {
       Accounts.createUser(admin);
+    } else {
+      Accounts.setPassword(exists._id, admin.password, { logout: false });
     }
   });
 });
@@ -35,7 +67,6 @@ const insertMissingValue = key => value => {
   }
 };
 bootstrapFunctions.push(() => {
-  let order = 0;
   _.each(config.conditions, (definition, key) => {
     if (definition.allowedValues) {
       definition.allowedValues.forEach(insertMissingValue(key));
