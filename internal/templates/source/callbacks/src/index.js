ca08f11d1c7b03b54f4d6b9de8ea5e77f53931e6
Nicolas Paton <nicolas.paton@gmail.com>
2022-03-13 13:31:46 +0800

    fix: experiment open set at the right time

--- internal/templates/source/callbacks/src/index.js
+++ internal/templates/source/callbacks/src/index.js
@@ -1,8 +1,8 @@
+import { connect, StandardCallbacks } from "@empirica/admin";
 import callbacks from "./callbacks.js";
-import { DefaultCallbacks, connect } from "@empirica/admin";
 
 (async () => {
   // Connect will block until the program is asked to stop (SIGINT).
-  await connect({ cbs: callbacks.merge(DefaultCallbacks) });
+  await connect({ cbs: callbacks.merge(StandardCallbacks) });
   process.exit(0);
 })();
---
+++ lib/go.mod
@@ -0,0 +1,3 @@
+module fake_go_module // is used to not try to find go files in node_modules
+
+go 1.14
