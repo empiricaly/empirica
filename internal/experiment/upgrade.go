24f74a3e33930e2fd5e731ea14607ac4ccdbd647
Nicolas Paton <nicolas.paton@gmail.com>
2022-09-20 14:59:42 +0800

    feat: make core upgrade a fixed version

--- internal/experiment/upgrade.go
+++ internal/experiment/upgrade.go
@@ -9,11 +9,11 @@ import (
 func Upgrade(ctx context.Context, playerPath, callbacksPath string) error {
 	stop := ShowSpinner("Upgrade empirica packages")
 
-	if err := RunCmdSilent(ctx, playerPath, "npm", "install", "--silent", "@empirica/core@latest"); err != nil {
+	if err := RunCmdSilent(ctx, playerPath, "npm", "install", "--silent", "-E", "@empirica/core@latest"); err != nil {
 		return errors.Wrap(err, "upgrade client")
 	}
 
-	if err := RunCmdSilent(ctx, callbacksPath, "npm", "install", "--silent", "@empirica/core@latest"); err != nil {
+	if err := RunCmdSilent(ctx, callbacksPath, "npm", "install", "--silent", "-E", "@empirica/core@latest"); err != nil {
 		return errors.Wrap(err, "upgrade server")
 	}
 
