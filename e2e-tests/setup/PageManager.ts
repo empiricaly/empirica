d34bbb315053b273993b2777adc85e21e4674551
Alexey Zaslavsky <pterolex@gmail.com>
2023-02-02 00:19:38 +0200

    Cleanup

--- e2e-tests/setup/PageManager.ts
+++ e2e-tests/setup/PageManager.ts
@@ -1,61 +1,30 @@
-/* eslint-disable   */
-/* eslint-disable */
-import { promises as fs, constants } from "fs";
-import * as path from "path";
-import * as os from "os";
-import * as uuid from "uuid";
-import * as childProcess from "node:child_process";
-import { type Page, type Browser, type BrowserContext } from "@playwright/test";
+import { type Browser } from "@playwright/test";
 
-import * as tar from "tar";
-import executeCommand from "../utils/launchProcess";
-import {
-  EmpiricaVersion,
-  parseBranchName,
-  parseBuild,
-  parseVersion,
-} from "../utils/versionUtils";
-import BasePage, { BasePageConstructorInterface, BasePageInterface } from "../page-objects/BasePage";
-
-const EMPIRICA_CMD = "empirica";
-const EMPIRICA_CONFIG_RELATIVE_PATH = path.join(".empirica", "local");
-
-const EMPIRICA_CORE_PACKAGE_PATH = path.join(
-  __dirname,
-  "..",
-  "..",
-  "lib",
-  "@empirica",
-  "core"
-);
-
-const CACHE_FOLDER = "cache";
-
-interface TestFactoryParams {
-  shouldBuildCorePackage: boolean;
-  shoudLinkCoreLib: boolean;
-}
-
+import BasePage from "../page-objects/BasePage";
 
 export default class PageManager {
   private pages: BasePage[];
 
-  constructor(params?: TestFactoryParams) {
+  constructor() {
     this.pages = [];
   }
 
-  public createPage<T extends BasePage>(pageClass: new (...a: any[]) => T, {browser, baseUrl}: { browser: Browser, baseUrl?: string}): T {
-    const page  = new pageClass({
+  // This method should accept a class that is inherited from the BasePage class and return the instance of it
+  public createPage<T extends BasePage>(
+    pageClass: new (...a: any[]) => T,
+    { browser, baseUrl }: { browser: Browser; baseUrl?: string }
+  ): T {
+    // eslint-disable-next-line new-cap
+    const page = new pageClass({
       browser,
-      baseUrl
-    })
+      baseUrl,
+    });
 
     this.pages.push(page);
 
     return page;
   }
 
- 
   public async cleanup() {
     for (const page of this.pages) {
       await page.close();
