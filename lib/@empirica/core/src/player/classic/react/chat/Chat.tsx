c52fce3028e1d2455fe604e8d757b821c6326fc0
Nicolas Paton <nicolas.paton@gmail.com>
2023-04-16 09:56:37 +0000

    feat: input component

--- lib/@empirica/core/src/player/classic/react/chat/Chat.tsx
+++ lib/@empirica/core/src/player/classic/react/chat/Chat.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FormEvent, KeyboardEvent, MouseEvent, useState } from "react";
 import { Attributable } from "../../../../shared/scopes";
 import { Loading } from "../../../react";
 import { WithChildren } from "../../../react/helpers";
@@ -6,15 +6,41 @@ import { usePlayer } from "../hooks";
 
 export type ChatProps = WithChildren<{
   scope: Attributable;
-  key: string;
+  scopeKey: string;
   loading?: React.ElementType;
 }>;
 
 export function Chat({
   scope,
-  key = "messages",
+  scopeKey = "messages",
   loading: LoadingComp = Loading,
 }: ChatProps) {
+  const handleNewMessage = (msg: string) => {
+    console.log("New Message:", msg);
+  };
+
+  return (
+    <div className="h-full w-full flex flex-col">
+      <div className="h-full">
+        <Messages scope={scope} scopeKey={scopeKey} loading={LoadingComp} />
+      </div>
+
+      <Input onNewMessage={handleNewMessage} />
+    </div>
+  );
+}
+
+type MessageProps = WithChildren<{
+  scope: Attributable;
+  scopeKey: string;
+  loading?: React.ElementType;
+}>;
+
+function Messages({
+  scope,
+  scopeKey = "messages",
+  loading: LoadingComp = Loading,
+}: MessageProps) {
   const player = usePlayer();
 
   if (!scope || !player) {
@@ -22,37 +48,87 @@ export function Chat({
   }
 
   player.getAttribute("avatar");
-  const msgs = scope.getAttribute(key);
+  const msgs = scope.getAttribute(scopeKey);
 
   console.log(msgs);
 
-  return (
-    <div className="h-full w-full flex flex-col">
-      <div className="h-full">Messages</div>
-
-      <Input />
-    </div>
-  );
+  return null;
 }
 
-export function Messages() {}
-export function Input() {
+type InputProps = WithChildren<{
+  onNewMessage: (msg: string) => void;
+}>;
+
+function Input({ onNewMessage }: InputProps) {
+  const [text, setText] = useState("");
+
+  const resize = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    const target = e.target as HTMLTextAreaElement;
+    target.style.height = "inherit";
+    target.style.height = `${Math.min(target.scrollHeight, 200)}px`;
+  };
+
+  const handleSubmit = (
+    e:
+      | FormEvent<HTMLFormElement>
+      | KeyboardEvent<HTMLTextAreaElement>
+      | MouseEvent<HTMLButtonElement>
+  ) => {
+    e.preventDefault();
+
+    const txt = text.trim();
+    if (txt === "") {
+      return;
+    }
+
+    if (txt.length > 1024) {
+      e.preventDefault();
+
+      alert("Max message length is 1024");
+
+      return;
+    }
+
+    onNewMessage(txt);
+    setText("");
+  };
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    console.log(e.key);
+    if (e.key === "Enter" && e.shiftKey === false) {
+      handleSubmit(e);
+      resize(e);
+    }
+  };
+
+  const handleKeyUp = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    resize(e);
+  };
+
   return (
-    <div className="p-2 flex items-strech gap-1">
-      <input
-        type="text"
+    <form
+      className="p-2 flex items-strech gap-2 border-t"
+      onSubmit={handleSubmit}
+    >
+      <textarea
         name="message"
         id="message"
-        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-200 placeholder:text-gray-300 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+        rows={1}
+        className="peer resize-none bg-transparent block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-200 placeholder:text-gray-300 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
         placeholder="Say something"
+        onKeyDown={handleKeyDown}
+        onKeyUp={handleKeyUp}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
       />
 
       <button
         type="button"
         className="rounded-md bg-gray-300 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+        onClick={handleSubmit}
       >
         Send
       </button>
-    </div>
+    </form>
   );
 }
